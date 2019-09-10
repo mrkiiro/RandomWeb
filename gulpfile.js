bb31dce0265d0e416aea3fc09dd78c26d64fa5a2
mrkiiro <kiiro.crafter@gmail.com>
2019-09-10 15:52:41 +0200

    automated saving of html files
    Martin er sej

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,20 @@
 gulp = require('gulp');
+browsersync = require('browser-sync').create();
+
+gulp.task('default', async function(){
+    browsersync.init({
+        server: {
+            baseDir: "dist/"
+        }
+    });
+
+    gulp.watch('./dev/*').on('change', gulp.series('build', 'reload'));
+});
+
+gulp.task('reload', function(){
+    return browsersync.reload();
+})
+
 
 gulp.task('build', function(){
     return gulp.src('dev/*.html')
